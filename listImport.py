f2e18adcac683491e041358d2ff82abc59b2769d
Sikayl <80312393+CkyleCasumpang@users.noreply.github.com>
2021-05-13 22:58:59 +0800

    Add files via upload

--- listImport.py
+++ listImport.py
@@ -1,52 +1,8 @@
+from animeClass import anime
+
 #imports
 import sqlite3
-class anime:
-    def __init__(self, name, atype, genre, rating,
-     episodes):
-        self.name = name
-        self.type = atype
-        self.genre = genre
-        self.rating = rating
-        self.episodes = episodes
 
-    def getName(self):
-        return self.name
-
-    def setName(self,name):
-        self.name = name
-
-    def getType(self):
-        return self.type
-
-    def setType(self,atype):
-        self.type = atype
-
-    def getGenre(self):
-        return self.genre
-
-    def setGenre(self,genre):
-        self.genre = genre
-    
-    def getRating(self):
-        return self.rating
-
-    def setRating(self,rating):
-        self.rating = rating
-    
-    def getEpisodes(self):
-        return self.episodes
-
-    def setEpisodes(self,episodes):
-        self.episodes = episodes
-
-    def printInfo(self):
-        print (
-            " Name: ", self.name, '\n'
-            ,"Type: ", self.type, '\n'
-            ,"Genre: ", self.genre, '\n'
-            ,"Rating: ", self.rating, '\n'
-            ,"Episodes:", self.episodes, '\n'
-        )
 
 conn = sqlite3.connect('anime_sql.db')
 print ('connection complete between python and sqlite3...')
